73d0b411051b7a34290efc9aa5609642f32ac4cc
Brian <legobmw99@gmail.com>
2023-03-25 11:17:29 -0400

    Update to 1.18 (#1)

--- src/main/java/com/legobmw99/oldobsidian/OldObsidian.java
+++ src/main/java/com/legobmw99/oldobsidian/OldObsidian.java
@@ -1,55 +1,46 @@
 package com.legobmw99.oldobsidian;
 
-
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRedstoneWire;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.world.World;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RedStoneWireBlock;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.BlockEvent.NeighborNotifyEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = OldObsidian.MODID, version = OldObsidian.VERSION)
+
+@Mod("oldobsidian")
 public class OldObsidian {
-	public static final String MODID = "oldobsidian";
-	public static final String VERSION = "1.1";
-	public static Block blockWetstone;
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		MinecraftForge.EVENT_BUS.register(this);
-	}
-	
-	@SubscribeEvent
-	public void onNotify(NeighborNotifyEvent e) {
-		World world = e.getWorld();
-		if(world.getBlockState(e.getPos()).getMaterial() == Material.LAVA) { //Make sure block updating is lava
-			for(EnumFacing side1: EnumFacing.VALUES) { //Iterate through relevant sides
-				IBlockState state = world.getBlockState(e.getPos().offset(side1));
-				if (state.getBlock() == Blocks.REDSTONE_WIRE 
-					&& state.getValue(BlockRedstoneWire.POWER) == 0) { //See if they are unpowered redstone
-					for(EnumFacing side2: EnumFacing.HORIZONTALS) { //Iterate through the horizontal sides of the redstone
-						//Check if water is present
-						if(world.getBlockState(e.getPos().offset(side1).offset(side2)).getMaterial() == Material.WATER) {
-							//Set the block to obsidian and play an effect
-							world.setBlockState(e.getPos().offset(side1), Blocks.OBSIDIAN.getDefaultState(), 2);
-							world.playSound((EntityPlayer) null, e.getPos(), SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.BLOCKS, 0.5F,
-									2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
-							//No need to keep checking this block
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
+    public OldObsidian() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onNotify(BlockEvent.NeighborNotifyEvent e) {
+        var world = e.getWorld();
+        if (world.getBlockState(e.getPos()).getMaterial() == Material.LAVA) { //Make sure block updating is lava
+            for (Direction side1 : Direction.values()) { //Iterate through relevant sides
+                var state = world.getBlockState(e.getPos().relative(side1));
+                if (state.getBlock() == Blocks.REDSTONE_WIRE && state.getValue(RedStoneWireBlock.POWER) == 0) { //See if they are unpowered redstone
+                    //Iterate through the horizontal sides of the redstone
+                    for (Direction side2 : Direction.Plane.HORIZONTAL) {
+                        //Check if water is present
+                        if (world.getBlockState(e.getPos().relative(side1).relative(side2)).getMaterial() == Material.WATER) {
+                            //Set the block to obsidian and play an effect
+                            world.setBlock(e.getPos().relative(side1), Blocks.OBSIDIAN.defaultBlockState(), 2);
+                            world.playSound(null, e.getPos(), SoundEvents.LAVA_EXTINGUISH, SoundSource.BLOCKS, 0.5F,
+                                            2.6F + (world.getRandom().nextFloat() - world.getRandom().nextFloat()) * 0.8F);
+                            //No need to keep checking this block
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
